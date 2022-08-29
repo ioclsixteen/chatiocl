eb8b9f16ec0d0cef15943250ba16c7d156561fbf
ioclsixteen <111957631+ioclsixteen@users.noreply.github.com>
2022-08-29 10:12:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,140 +1,26 @@
 const express = require("express");
-//const { dialogflow } = require("actions-on-google");
-const dialogflow = require('@google-cloud/dialogflow');
-const uuid = require('uuid');
-const https = require('https');
-const fs = require('fs');
-const express = require("express");
 
 const app = express();
-app.use(express.json());
-
-//const config = require("./config/keys");
+app.use(express.json())
 
 
-app.get('/',function(req,res) {
-    res.send({"quote":"Personality is to a man what perfume is to a flower."});
-        })
-    })
+app.get("/",function(request,response) {
+response.send({"quote":"Personality is to a man what perfume is to a flower."});
 });
 
 app.get("/about",function(request,response) {
-  res.send("About");
+    response.send("About Page")
 })
-
-const CREDENTIALS = JSON.parse(fs.readFileSync('C:/Users/00516016/Documents/my-express-server/order-boqx-9963a9b13beb.json'));
-
-
-const PROJECT_ID = CREDENTIALS.project_id;
-const CONFIGURATION = {
-    credentials: {
-        private_key:CREDENTIALS.private_key,
-        client_email:CREDENTIALS.client_email
-    }
-};
-
-app.post('/webhook',async function(req,res){
-    // [START dialogflow_detect_intent_text]
-  
-    /**
-     * TODO(developer): UPDATE these variables before running the sample.
-     */
-    // projectId: ID of the GCP project where Dialogflow agent is deployed
-    // const projectId = 'PROJECT_ID';
-    // sessionId: String representing a random number or hashed user identifier
-    // const sessionId = '123456';
-    // queries: A set of sequential queries to be send to Dialogflow agent for Intent Detection
-    // const queries = [
-    //   'Reserve a meeting room in Toronto office, there will be 5 of us',
-    //   'Next monday at 3pm for 1 hour, please', // Tell the bot when the meeting is taking place
-    //   'B'  // Rooms are defined on the Dialogflow agent, default options are A, B, or C
-    // ]
-    // languageCode: Indicates the language Dialogflow agent should use to detect intents
-    // const languageCode = 'en';
-  
-
-    // Instantiates a session client
-    const sessionClient = new dialogflow.SessionsClient(CONFIGURATION);
-    
-      // The path to identify the agent that owns the created intent.
-      const sessionId = uuid.v4();
-      const sessionPath = sessionClient.projectAgentSessionPath(
-        'order-boqx',
-        sessionId,
-      );
-      console.log(sessionId);
-  
-      // The text query request.
-      const request = {
-        session: sessionPath,
-        queryInput: {
-          text: {
-            text: req.body.text,
-            languageCode: 'en',
-          },
-        },
-      };
-      
-      console.log(request);
-      const responses = await sessionClient.detectIntent(request);
-      console.log(responses);
-      console.log('Detected intent:');
-      const result = responses[0].queryResult;
-
-      console.log(`  Query: ${result.queryText}`);
-      console.log(`  Response: ${result.fulfillmentText}`);
-      if (result.intent) {
-        console.log(`  Intent: ${result.intent.displayName}`);
-      } else {
-        console.log('  No intent matched.');
-      }
-      res.send(responses);
-     }
-);
-
-// const PROJECTID = CREDENTIALS.project_id;
-// app.post('/webhook',async function(req,res) {
-
-//     const sessionId = uuid.v4();
-//     const sessionClient = new dialogflow.SessionsClient(CONFIGURATION);
-//     const sessionPath = sessionClient.projectAgentSessionPath(PROJECTID,sessionId);
-//         // The text query request.
-//     const request = {
-//         session: sessionPath,
-//         queryInput: {
-//         text: {
-//             // The query to send to the dialogflow agent
-//             text: req.body.text,
-//             // The language used by the client (en-US)
-//             languageCode: 'en',
-//         },
-//         },
-//     };
-    
-//     // Send request and log result
-//     const responses = await sessionClient.detectIntent(request);
-//     console.log('Detected intent');
-//     const result = responses[0].queryResult;
-//     console.log(`  Query: ${result.queryText}`);
-//     console.log(`  Response: ${result.fulfillmentText}`);
-//     if (result.intent) {
-//         console.log(`  Intent: ${result.intent.displayName}`);
-//     } else {
-//         console.log(`  No intent matched.`);
-//     }
-
-//     res.send(result)
-// })
 
 
 app.post('/dialogflow',function(request,response) {
-    let agent = new WebhookClient({request: request,response: response});
+let agent = new WebhookClient({request: request,response: response});
 
-    let intentMap = new Map();
+let intentMap = new Map();
 
-    intentMap.set('Sum',handleWebHookIntent);
+intentMap.set('Sum',handleWebHookIntent);
 
-    agent.handleRequest(intentMap);
+agent.handleRequest(intentMap);
 });
 
 
@@ -143,9 +29,8 @@ function handleWebHookIntent(agent) {
 }
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT,function() {
- console.log("Server is running on port "+ PORT);
+ console.log("Server is running on port 3000");
 });
-
 
